6f1eb1218dc13238ad1770faf20950770433601a
andrijanac <andrijanacanic2000@gmail.com>
2024-12-06 01:52:24 +0100

    Added JSON database with basic read/write functionality.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject personal-finance-tracker "0.1.0-SNAPSHOT"
   :description "An application designed to track and manage personal finances."
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [cheshire "5.10.0"]]
   :main personal-finance-tracker.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/personal_finance_tracker/core.clj
+++ src/personal_finance_tracker/core.clj
@@ -1,4 +1,17 @@
-(ns personal-finance-tracker.core)
+(ns personal-finance-tracker.core
+  (:require [cheshire.core :as json]
+            [clojure.java.io :as io]))
+
+(def db-path "src/personal_finance_tracker/my-budget.json")
+
+(defn read-db []
+  (with-open [reader (io/reader db-path)]
+    (json/parse-string (slurp reader) true)))
+
+(defn write-db [data]
+  (with-open [writer (io/writer db-path)]
+    (.write writer (json/generate-string data))))
 
 (defn -main []
-  (println "Welcome to Personal Finance Tracker!"))
+  (println "Welcome to Personal Finance Tracker!")
+  (println "Database content: " (read-db)))
